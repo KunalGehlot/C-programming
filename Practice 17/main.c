f3410f009326cd0789ce01f26bf2acc8997505c5
Zackbare <gehlotkunal4@gmail.com>
2018-03-25 14:03:07 +0530

    P doesn't make any problems now

---
+++ Practice 17/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 24-Nov-2017
+ *      Author: Kunal Gehlot
+ */
+#include<stdio.h>
+
+int main() {
+
+	for (int i = 1; i < 20; i += 2) {
+
+		printf("%d\n", i);
+	}
+}
+
